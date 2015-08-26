f90f1db03f3c7cd8de33d46fc6e2f7100c010458
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-08-26 22:12:33 +0100

    Add tests package

---
+++ OpenClosedPrinciple/src/siemens/plm/openclosed/dicegame/tests/ScorerTests.java
@@ -0,0 +1,14 @@
+package siemens.plm.openclosed.dicegame.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScorerTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
