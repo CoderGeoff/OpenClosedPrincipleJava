0acf1cd923b0777ee8d1da72c9d83715461594e0
Geoff Bones <geoff.bones@siemens.com>
2015-08-27 12:00:34 +0100

    Change to use array instead of map

--- OpenClosedPrinciple/src/siemens/plm/openclosed/dicegame/Scorer.java
+++ OpenClosedPrinciple/src/siemens/plm/openclosed/dicegame/Scorer.java
@@ -1,30 +1,27 @@
 package siemens.plm.openclosed.dicegame;
 
 import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Scorer implements IScorer {
-	private Map<Integer, Integer> diceCount;
 
 	@Override
 	public int Score(List<Integer> diceThrows) {
 	    int score = 0;
-	    Map<Integer, Integer> diceCount = new HashMap<Integer, Integer>();
-	    Map<Integer, Integer> subtractions = new HashMap<Integer, Integer>();
+	    int[] diceCount = new int[7];
+	    int[] subtractions = new int[7];
 		
-		diceThrows.stream().forEach(value -> incrementEntry(diceCount, value, 1));
+		diceThrows.stream().forEach(value -> diceCount[value]++);
 
 	    boolean fullStraight = true;
 	    for (int diceThrow = 1; diceThrow <= 6; ++diceThrow)
 	    {
-	        fullStraight = fullStraight && getEntry(diceCount, diceThrow) - getEntry(subtractions, diceThrow) == 1;
+	        fullStraight = fullStraight && diceCount[diceThrow] - subtractions[diceThrow] == 1;
 	    }
-	    if (fullStraight)
+	    if (fullStraight) 
 	    {
 	        for (int diceThrow = 1; diceThrow <= 6; ++diceThrow)
 	        {
-	            incrementEntry(subtractions, diceThrow, 1);
+	            subtractions[diceThrow]++;
 	        }
 	        score += 1500;
 	    }
@@ -32,28 +29,18 @@ public class Scorer implements IScorer {
 	    for (int diceThrow = 1; diceThrow <= 6; ++diceThrow)
 	    {
 	        int scoreMultiplier = diceThrow == 1 ? 10 : diceThrow;
-	        int numberOfDice = getEntry(diceCount, diceThrow) - getEntry(subtractions, diceThrow);
+	        int numberOfDice = diceCount[diceThrow] - subtractions[diceThrow];
 	        if (numberOfDice >= 3)
 	        {
 	            score += (100 * scoreMultiplier) * Math.pow(2, numberOfDice - 3);
-	            incrementEntry(subtractions, diceThrow, numberOfDice);
+	            subtractions[diceThrow] += numberOfDice;
 	        }
 	    }
 
-	    score += (getEntry(diceCount, 5) - subtractions.getOrDefault(5, 0)) * 50;
-	    score += (getEntry(diceCount, 1) - subtractions.getOrDefault(1, 0)) * 100;
+	    score += (diceCount[5] - subtractions[5]) * 50;
+	    score += (diceCount[1] - subtractions[1]) * 100;
 
 	    return score;
 
 	}
-	
-	private Integer incrementEntry(Map<Integer, Integer> map, Integer key, int increment) {
-		return map.put(key, getEntry(map, key) + increment);
-	}
-
-	private Integer getEntry(Map<Integer, Integer> map, Integer key) {
-		return map.getOrDefault(key, 0);
-	}
-	
-	
 }
