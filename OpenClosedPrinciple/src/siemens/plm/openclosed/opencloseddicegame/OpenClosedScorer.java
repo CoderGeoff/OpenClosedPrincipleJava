3115e9f0bb505dfe6236188a8a8cb539851a329f
Geoff Bones <geoff.bones@siemens.com>
2015-08-27 12:42:56 +0100

    Add open closed scorer

---
+++ OpenClosedPrinciple/src/siemens/plm/openclosed/opencloseddicegame/OpenClosedScorer.java
@@ -0,0 +1,38 @@
+package siemens.plm.openclosed.opencloseddicegame;
+
+import java.util.List;
+
+import siemens.plm.openclosed.dicegame.IScorer;
+
+
+public class OpenClosedScorer implements IScorer {
+
+	private List<IScoringRule> rules;
+
+	public OpenClosedScorer(List<IScoringRule> rules) {
+		this.rules = rules;
+	}
+
+	@Override
+	public int Score(List<Integer> diceThrows) {
+	    int score = 0;
+	    int[] diceCount = countDice(diceThrows);
+	    
+	    for (IScoringRule rule : rules)
+	    {
+	        for (ScoringRuleResult ruleResult = rule.tryMatch(diceCount); ruleResult.isMatched(); ruleResult = rule.tryMatch(diceCount))
+	        {
+	            score += ruleResult.getScore();
+	            diceCount = ruleResult.getRemainingDiceCount();
+	        }
+	    }
+	    return score;
+	}
+
+	private int[] countDice(List<Integer> diceThrows) {
+		int[] count = new int[7];
+		diceThrows.stream().forEach(n -> count[n]++);
+		return count;
+	}
+
+}
